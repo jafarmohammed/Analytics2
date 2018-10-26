76c974098baa11707d4e734adeb65ac720ec6013
jafarmohammed <jaafarmuhammad12@gmail.com>
2018-10-26 17:53:55 +0530

    26/10

--- data2/file11.R
+++ data2/file11.R
@@ -5,5 +5,11 @@ model <- lm(mpg ~ disp + hp + wt + qsec, data = mtcars)
 k <- ols_step_all_possible(model)
 plot(k)
 k
+fit3=lm(mpg~hp+wt+qsec,data=mtcars)
+summary(fit3)
 summary(lm(mpg ~ wt, data=mtcars))
 summary(lm(mpg ~ wt+ hp, data=mtcars))
+AIC(fit3)
+fit4 = lm(mpg ~.,data=mtcars)
+
+#discrete category 
